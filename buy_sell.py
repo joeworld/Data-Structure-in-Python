1b61e8220f4cad5c9999dcd58dafc11a54948a60
Joestack <iamjoestack@gmail.com>
2019-10-29 16:03:32 +0100

    Best Time to Buy and Sell Stock

--- buy_sell.py
+++ buy_sell.py
@@ -1,9 +1,7 @@
 """
-Given a list of stock prices for a given day, your goal is to
-return the maximum profit that could have been made by buying a
-stock later on.
-[45, 24, 35, 31, 40, 38, 11] should return 16 cos the highest profit to
-be made is 16 when you buy a stock for $24 and sell $40
+Say you have an array for which the ith element is the price of a given stock on day i.
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+Note that you cannot sell a stock before you buy one.
 """
 A = [45, 24, 35, 31, 40]
 
